847c6f4fc8bc880b265c8942730a96369343b81c
k_czech <kamczeh@gmail.com>
2024-08-19 08:25:48 +0200

    removed no user mocks and environment variables

--- config.js
+++ config.js
@@ -2,6 +2,4 @@ module.exports = {
 	BASE_URL: 'https://api.github.com',
 	ACCESS_TOKEN:
 		'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',
-	TOAST_DURATION: 2000,
-	TOAST_PLACEMENT: 'top',
 };
